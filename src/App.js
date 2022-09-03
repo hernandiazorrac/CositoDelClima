4ad8d05d5623e0279a52b70c2902d006ae721ebe
Hernán Díaz Orrac <hernandiazorrac@gmail.com>
2022-09-03 17:10:34 -0300

    searchbar y background

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import './SearchBar/SearchBar'
+import SearchBar from './SearchBar/SearchBar';
+
+const api = {
+  key: "93afee0008cb9b8ff8daf84106c6978c",
+  base: "https://api.openweathermap.org/data/3.0"
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <SearchBar />
+      </main>
     </div>
   );
 }
---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import './SearchBar.css'
+
+const SearchBar = () => {
+return(
+    <div className="searchBox">
+      <input type="text" className="searchBar" placeholder="Buscar ciudad..." />
+    </div>
+)
+}
+
+export default SearchBar;
